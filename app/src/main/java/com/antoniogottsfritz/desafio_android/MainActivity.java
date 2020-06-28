2b64e5f77fdf4763095561497e6ab5ae5467a343
Antonio Gottsfritz <antoniobgott@gmail.com>
2020-06-28 00:20:16 -0300

    Initial commit

---
+++ app/src/main/java/com/antoniogottsfritz/desafio_android/MainActivity.java
@@ -0,0 +1,25 @@
+package com.antoniogottsfritz.desafio_android;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
